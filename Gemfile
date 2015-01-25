4965daa2a1d057a6d63762f891eded474d7276de
Sylwia Olak <pumpkincouture@gmail.com>
2015-01-25 14:19:08 -0600

    Updated views and pages

--- Gemfile
+++ Gemfile
@@ -1,12 +1,16 @@
 source 'https://rubygems.org'
 
-gem 'rspec', '3.0.0'
 gem 'sinatra', '1.4.5'
 gem 'data_mapper', '1.2.0'
-group :development do 
-  gem 'sqlite3', '~> 1.3.10'	
-  gem 'rvm'
-end
-# gem 'do_postgres' '0.10.14'
-# gem 'dm-postgres-adapter', :group => :production
-# gem 'dm-sqlite-adapter', :group => :development
+
+# group :development, :test do
+#   gem 'sqlite3', '~> 1.3.10'
+#   gem 'rspec', '3.0.0'
+# end
+# group :production do
+#   gem 'pg'
+# end
+
+gem 'dm-postgres-adapter', :group => :production
+gem 'dm-sqlite-adapter', :group => :development
+
--- time_logger.rb
+++ time_logger.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'data_mapper'
+require_relative 'lib/logger_constants.rb'
+require 'date'
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/time_logger.db")
+include LoggerConstants
 
 class Employee
   include DataMapper::Resource
@@ -37,18 +40,96 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 get '/' do
-  @employees = Employee.all 
   @title = "Time Logger"
   erb :home
 end
 
-post '/form' do
-	@employees = Employee.all(username: params[:username])
-	p "found: #{ @employees.map { |e| e.first + e.last }}"
-	if @employees.empty?
-		"You're not an employee, '#{params[:username]}'"
-	else
-	  erb :form
+post '/options' do
+	return_employee
+	if return_employee.empty?
+		erb :home_username_failed
+	elsif return_employee.first(:type => 'admin') 
+		erb :admin_form
+	else 
+		erb :non_admin_form
 	end
 end
+
+post '/selection' do
+   @clients = Client.all
+   if params[:admin]
+   	 	choice = call_constants(params[:admin])
+   		choose_form(choice)
+   elsif params[:nonadmin]
+   		choice = call_constants(params[:nonadmin])
+   		choose_form(choice)
+   end
+end
+
+get '/add_employee' do
+	erb :add_employee
+end
+
+get '/add_client' do
+	erb :add_client
+end
+
+post '/add_employee' do 
+	until verify_fields([params[:first], params[:last], params[:username], params[:employee_type]])
+		redirect '/add_employee'
+	end
+	erb :add_employee_success
+end
+
+post '/add_client' do
+	until verify_fields([params[:client_name], params[:client_type]])
+		redirect '/add_client'
+	end
+	erb :add_client_success
+end
+
+private
+
+def choose_form(choice)
+  case choice
+  when CHOICE_1
+  	erb :log_time
+  when CHOICE_2
+  	@time_sheet= TimeSheet.all(:month => get_month) & TimeSheet.all(:year =>get_year)
+  	erb :time_report
+  when CHOICE_3
+  	erb :add_employee
+  when CHOICE_4
+  	erb :add_client
+  when CHOICE_5
+  	erb :employee_report
+  end
+end
+
+def return_employee
+ @employees = Employee.all(username: params[:username])
+end
+
+def verify_fields(params)
+	params.each do |param|
+		if param == nil || param.empty? 
+			return false		
+		end
+	end
+end
+
+def call_constants(params)
+	ADMIN_OPTIONS.each {|number, option| return number.to_s if params == number.to_s}
+end
+
+def get_month
+	Date.today.month.to_s
+end
+
+def get_year
+	Date.today.year.to_s
+end
+
+
+
 
